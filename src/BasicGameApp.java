d6f95ef6c8dd8014336b76314e8a1a889bb3ad5e
gavin217 <gavin_mcbrine28@milton.edu>
2026-05-08 14:57:18 -0400

    stuff

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -3,4 +3,17 @@ public class BasicGameApp {
     public boolean gameOn;
     public Player p1;
     public Dealer d1;
+
+    public static void main(String[] args) {
+BasicGameApp a= new BasicGameApp();
+    }
+    public BasicGameApp(){
+        System.out.println("welcome to blackjack");
+        deck=new Card[52];
+        for (int x=0;x<13;x++){
+            deck[x]=new Card("hearts",10,x);
+            deck[x].printInfo();
+
+        }
+    }
 }
--- src/Card.java
+++ src/Card.java
@@ -4,5 +4,19 @@ public class Card {
     public int value;
     public String name;
     public String suit;
-    public Card(){}
+    public Card(String pSuit, int pValue, int pName){
+        if(pName==11){
+
+        name="ace";
+        value=11;
+        } else if(pName==1) {
+            name="two";
+            value=2;
+        }
+        suit=pSuit;
+        value=pValue;
+    }
+    public void printInfo(){
+        System.out.println("the "+name+" of "+suit+" has a value of "+value);
+    }
 }
