8a2a590c7d0a3b935ef5711afde4eb14be4e31d6
gavin217 <gavin_mcbrine28@milton.edu>
2026-05-13 09:47:44 -0400

    printed whole deck

--- src/BasicGameApp.java
+++ src/BasicGameApp.java
@@ -10,8 +10,28 @@ BasicGameApp a= new BasicGameApp();
     public BasicGameApp(){
         System.out.println("welcome to blackjack");
         deck=new Card[52];
+        printDeck();
+
+
+    }
+    public void printDeck(){
         for (int x=0;x<13;x++){
             deck[x]=new Card("hearts",10,x);
+            deck[x].printInfo();
+
+        }
+        for (int x=0;x<13;x++){
+            deck[x]=new Card("diamonds",10,x);
+            deck[x].printInfo();
+
+        }
+        for (int x=0;x<13;x++){
+            deck[x]=new Card("clubs",10,x);
+            deck[x].printInfo();
+
+        }
+        for (int x=0;x<13;x++){
+            deck[x]=new Card("spades",10,x);
             deck[x].printInfo();
 
         }
--- src/Card.java
+++ src/Card.java
@@ -5,7 +5,8 @@ public class Card {
     public String name;
     public String suit;
     public Card(String pSuit, int pValue, int pName){
-        if(pName==11){
+        value=pValue;
+        if(pName==0){
 
         name="ace";
         value=11;
@@ -13,8 +14,53 @@ public class Card {
             name="two";
             value=2;
         }
-        suit=pSuit;
-        value=pValue;
+        else if(pName==2){
+            name="three";
+            value=3;
+        }
+        else if(pName==3){
+            name="four";
+            value=4;
+        }
+        else if(pName==4){
+            name="five";
+            value=5;
+        }
+        else if(pName==5){
+            name="six";
+            value=6;
+        }
+        else if(pName==6){
+            name="seven";
+            value=7;
+        }
+        else if(pName==7){
+            name="eight";
+            value=8;
+        }
+        else if(pName==8){
+            name="nine";
+            value=9;
+        }
+        else if(pName==9){
+            name="jack";
+            value=10;
+        }
+        else if(pName==10){
+            name="queen";
+            value=10;
+        }
+        else if(pName==11){
+            name="king";
+            value=10;
+        }
+        else if(pName==12){
+            name="ace";
+            value=1;
+        }
+    suit=pSuit;
+
+
     }
     public void printInfo(){
         System.out.println("the "+name+" of "+suit+" has a value of "+value);
